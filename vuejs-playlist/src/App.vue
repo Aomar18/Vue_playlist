e981836d8ee681031fe194e6358ca2d366580ed2
Ahmed <omar0121@umn.edu>
2018-10-29 13:32:35 -0500

    working on global nesting of components - local nesting working

---
+++ vuejs-playlist/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>  
+  
+  <h1> {{title}} </h1>
+  <ninjas></ninjas>
+  
+  
+</div>
+</template>
+<script>
+import Ninjas from './Ninjas.vue'
+
+
+export default {
+
+  name: 'app',
+  data () {
+    return
+    title: 'Ninja App'
+  }
+}
+</script>
+
+<style></style>
---
+++ vuejs-playlist/src/Ninjas.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="ninja in ninjas">{{ninja}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ninjas',
+  data() {
+    return {
+      ninjas: ["Yoshi", "Mario", "luigi"]
+    };
+  }
+};
+</script>
+
+<style>
+html {
+  background-color: darkgray;
+}
+</style>
