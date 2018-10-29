9adabfd72f792f1c1a79ed6d23feb05b8f1e446d
Ahmed <omar0121@umn.edu>
2018-10-29 14:02:16 -0500

    fixed rendering issue with title and styling to title text

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,24 +1,31 @@
 <template>
-<div>  
+  <div>
   
   <h1> {{title}} </h1>
   <ninjas></ninjas>
   
   
-</div>
-</template>
-<script>
-import Ninjas from './Ninjas.vue'
 
+  </div>
+</template>
+
+<script>
+import Ninjas from "./Ninjas.vue";
 
 export default {
-
-  name: 'app',
-  data () {
-    return
-    title: 'Ninja App'
+  components: {
+    ninjas: Ninjas
+  },
+  data() {
+    return{
+    title: "Ninja App"
+    }
   }
-}
+};
 </script>
 
-<style></style>
+<style>
+h1 {
+  color: purple;
+}
+</style>
